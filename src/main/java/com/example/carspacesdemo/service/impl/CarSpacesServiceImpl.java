2c956e3bb96029ad928e3e8eaf775f80c99b9d88
rico_guo <1476262983@qq.com>
2023-06-12 08:44:19 +0800

    add 取消collectors的引用

--- src/main/java/com/example/carspacesdemo/service/impl/CarSpacesServiceImpl.java
+++ src/main/java/com/example/carspacesdemo/service/impl/CarSpacesServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class CarSpacesServiceImpl extends ServiceImpl<CarspaceMapper, Carspace> implements CarSpacesService {
